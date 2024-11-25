f83f3d3090fb7bb8afeda81a5138def79bfa23e8
paulosoares7 <paulosoaresrodrigues@outlook.com>
2024-11-25 02:39:46 -0400

    menu Hamburger

--- src/Components/Navigate/index.tsx
+++ src/Components/Navigate/index.tsx
@@ -1,13 +1,29 @@
-export function Navigate (){
-    return (
-        <nav>
-            <ul>
-                <li><a href="#informacoes">Sobre</a></li>
-                <li><a href="#mapa">Hospitais</a></li>
-                <li><a href="#servicos">Serviços</a></li>
-                <li><a href="#faqs">FAQs</a></li>
-                <li><a href="#contato">Contato</a></li>
-            </ul>
-        </nav>
-    )
+import { useState } from 'react';
+import './styles.css'
+
+
+export function Navigate() {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
+  return (
+    <nav>
+      <button className={`menu-toggle ${menuOpen ? 'open' : ''}`} onClick={toggleMenu}>
+        <div></div>
+        <div></div>
+        <div></div>
+      </button>
+
+      <ul className={`nav-links ${menuOpen ? 'open' : ''}`}>
+        <li><a onClick={toggleMenu} href="#informacoes">Sobre</a></li>
+        <li><a onClick={toggleMenu} href="#mapa">Hospitais</a></li>
+        <li><a onClick={toggleMenu} href="#servicos">Serviços</a></li>
+        <li><a onClick={toggleMenu} href="#faqs">FAQs</a></li>
+        <li><a onClick={toggleMenu} href="#contato">Contato</a></li>
+      </ul>
+    </nav>
+  );
 }
